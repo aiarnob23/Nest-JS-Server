9fb2db561efaeb28ae7244fad34ec091bef25bcd
aiarnob23 <aiarnob23@gmail.com>
2026-04-29 08:45:32 +0600

    scalable design

--- src/modules/photo/photo.providers.ts
+++ src/modules/photo/photo.providers.ts
@@ -1,10 +1,11 @@
 import { DataSource } from "typeorm";
 import { Photo } from "./photo.entity";
+import { DATA_SOURCE, PHOTO_REPOSITORY } from "../../common/constants/token";
 
 export const photoProviers = [
     {
-        provide: 'PHOTO_REPOSITORY',
+        provide: PHOTO_REPOSITORY,
         useFactory: (dataSource: DataSource) => dataSource.getRepository(Photo),
-        inject: ['DATA_SOURCE'],
+        inject: [DATA_SOURCE],
     },
 ];
